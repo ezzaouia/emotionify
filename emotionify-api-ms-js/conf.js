9f29bd2ee243e00bcedf2c9dff5b94859f2f7cd6
Mohamed Ez zaouia <ezzaouia.mohamed@gmail.com>
2016-04-22 09:20:03 +0200

    refactoring port to 3003 & socket io

--- emotionify-api-ms-js/conf.js
+++ emotionify-api-ms-js/conf.js
@@ -4,7 +4,7 @@ const
     path = require('path')
 
 let APP_CONF = {
-    WEB_PORT: 3000,
+    WEB_PORT: 3003,
     MS_API_KEY: 'ad03ff6c15254bc28325d9217f30c9ba',
     MS_API_URL: 'https://api.projectoxford.ai/emotion/v1.0/recognize',
     CLIENT_PATH: path.join('..', 'emotionify-ui', 'client')
--- emotionify-api-ms-js/index.js
+++ emotionify-api-ms-js/index.js
@@ -33,7 +33,7 @@ let allowCrossDomain = function (req, res, next) {
 app.use(allowCrossDomain)
 
 
-app.use(express.static(path.join(__dirname, conf.CLIENT_PATH)));
+//app.use(express.static(path.join(__dirname, conf.CLIENT_PATH)));
 
 console.log(__dirname)
 
@@ -81,28 +81,55 @@ app.post('/emotionify_blob', rawParser, function (req, res) {
     })
 })
 
-io.on('connection', function (socket) {
-    socket.on('frame', function (data) {
-        // console.log(data.split(',')[1])
-        console.log(new Buffer(data.split(',')[1], 'base64'))
 
+/**
+ * Example of response from MS Emotion api
+ * ---------------------------------------
+ *
+ 200 [ { faceRectangle: { height: 137, left: 100, top: 79, width: 137 },
+ scores:
+ { anger: 0.0000544858121,
+ contempt: 0.004292106,
+ disgust: 0.000009244951,
+ fear: 0.00000439419364,
+ happiness: 0.00244169962,
+ neutral: 0.9925033,
+ sadness: 0.000192505817,
+ surprise: 0.0005022823 } } ]
+ */
+
+io.on('connection', function (socket) {
+    console.log('use connected')
+    console.log('use connected')
+    console.log('use connected')
+    console.log('use connected')
+
+    socket.on('frame', function (data) {
+        console.log(data.split(',')[1])
         let t0 = Date.now()
-        // request({
-        //     url: conf.MS_API_URL,
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/octet-stream',
-        //         'Ocp-Apim-Subscription-Key': conf.MS_API_KEY
-        //     },
-        //     body: new Buffer(data.split(',')[1], 'base64')
-        // }, function(error, response, body) {
-        //     if(error) {
-        //          console.log(error)
-        //     } else {
-        //          console.log(response.statusCode, body)
-        //     }
-        // })
+        request({
+            url: conf.MS_API_URL,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/octet-stream',
+                'Ocp-Apim-Subscription-Key': conf.MS_API_KEY
+            },
+            body: new Buffer(data.split(',')[1], 'base64')
+        }, function(error, response, body) {
+            if(error) {
+                 console.log(error)
+            } else {
+                console.log(JSON.parse(response.body)[0].scores)
+                socket.emit('oEmotionifiedFrame', JSON.parse(response.body)[0].scores)
+            }
+        })
+
+        // socket.emit('oEmotionifiedFrame', 'for testing')
     })
+   
+
+    //console.log(JSON.parse(body)[0].scores)
+    // socket.emit('oEmotionifiedFrame', 'for testing')
 })
     
 function timeDiff(t0) {
